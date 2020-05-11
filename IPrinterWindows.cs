682a5370f4b52228a75b2ed8f78fd657e8c25edf
SNuryahya <stya58@gmail.com>
2020-05-12 06:37:28 +0700

    Polymorphism Interface

---
+++ IPrinterWindows.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismPrinterInterface
+{
+    interface IPrinterWindows
+    {
+        void Show();
+        void Print();
+    }
+}
---
+++ LaserJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismPrinterInterface
+{
+    class LaserJet : IPrinterWindows
+    {
+        public void Print()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12 ");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("LaserJet printer printing.......");
+        }
+    }
+}
+    
+
